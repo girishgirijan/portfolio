b3da291ec063b154241b929c5ec8a4f14212a769
Girish Girijan <girishvazheliparambil@gmail.com>
2024-01-09 19:42:13 -0400

    Update in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
             <span>I'm Girish Girijan. </span>Full stack developer
           </h1>
           <p className="home__description">            
-            Over 4+ years of extensive work experience as a Full Stack Engineer and have proficient experience in designing and developing front-end and back-end technologies and restful web services in various industries like Utility, Insurance, Sports, Finance, and Recruitment.
+            Over 5+ years of extensive work experience as a Full Stack Engineer and have proficient experience in designing and developing front-end and back-end technologies and restful web services in various industries like Utility, Insurance, Sports, Finance, and Recruitment.
           </p>
 
           <Link to="/about" className='button'>
